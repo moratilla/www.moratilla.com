abbbf01e20274124f3dd0cbbd48b114c875ed746
Jorge Moratilla <jorge@moratilla.com>
2020-05-05 12:14:19 +0200

    ci: add test to check word spelling

---
+++ scripts/spell-checker.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+SPELL=`which aspell`
+E_FILE=$PWD/errors
+
+if [ -s ${E_FILE} ]
+then
+  echo "Clearing errors file."
+  rm ${E_FILE}
+fi
+
+if [ "${SPELL}" = "" ]
+then
+  echo "Package aspell is not installed.  Exitting..."
+  exit 1
+fi
+
+for i in `find . -name "*.md"`
+do
+  echo "File $i:" >> errors
+  ${SPELL} -M --home-dir=. list < $i >> ${E_FILE}
+done
+
+if [ -s ${E_FILE} ]
+then
+  echo "There are errors to fix.  Exitting..."
+  cat ${E_FILE}
+  exit 256
+fi
+
